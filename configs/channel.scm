0dae871a9f05775c769dd35bdc9872cf51c181be
ShineBreaker <102544159+ShineBreaker@users.noreply.github.com>
2026-01-05 00:26:53 +0800

    update.

--- configs/channel.scm
+++ configs/channel.scm
@@ -5,6 +5,15 @@
                   (inherit (car %default-channels))
                   (url "https://mirror.sjtu.edu.cn/git/guix.git"))
                 (channel
+                  (name 'cast)
+                  (branch "main")
+                  (url "https://codeberg.org/vnpower/cast")
+                  (introduction
+                   (make-channel-introduction
+                    "fab73647fd9f0f2167d9ef9d42cddd77500fffb3"
+                    (openpgp-fingerprint
+                     "D430 1F59 A65F 49DA 918A  3C0D 8D4C 3248 29DE D156"))))
+                (channel
                   (name 'nonguix)
                   (url "https://gitlab.com/nonguix/nonguix")
                   (introduction
@@ -12,15 +21,6 @@
                     "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
                     (openpgp-fingerprint
                      "2A39 3FFF 68F4 OPEF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-                (channel
-                  (name 'rosenthal)
-                  (url "https://codeberg.org/hako/rosenthal.git")
-                  (branch "trunk")
-                  (introduction
-                   (make-channel-introduction
-                    "7677db76330121a901604dfbad19077893865f35"
-                    (openpgp-fingerprint
-                     "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7"))))
                 (channel
                   (name 'radix)
                   (url "https://codeberg.org/anemofilia/radix.git")
@@ -31,14 +31,22 @@
                     (openpgp-fingerprint
                      "F164 709E 5FC7 B32B AEC7  9F37 1F2E 76AC E3F5 31C8"))))
                 (channel
-                  (name 'cast)
-                  (branch "main")
-                  (url "https://codeberg.org/vnpower/cast")
+                  (name 'rde)
+                  (url "https://git.sr.ht/~abcdw/rde")
                   (introduction
                    (make-channel-introduction
-                    "fab73647fd9f0f2167d9ef9d42cddd77500fffb3"
+                    "257cebd587b66e4d865b3537a9a88cccd7107c95"
                     (openpgp-fingerprint
-                     "D430 1F59 A65F 49DA 918A  3C0D 8D4C 3248 29DE D156"))))
+                     "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+                (channel
+                  (name 'rosenthal)
+                  (url "https://codeberg.org/hako/rosenthal.git")
+                  (branch "trunk")
+                  (introduction
+                   (make-channel-introduction
+                    "7677db76330121a901604dfbad19077893865f35"
+                    (openpgp-fingerprint
+                     "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7"))))
                 (channel
                   (name 'saayix)
                   (branch "main")
@@ -47,6 +55,15 @@
                    (make-channel-introduction
                     "12540f593092e9a177eb8a974a57bb4892327752"
                     (openpgp-fingerprint
-                     "3FFA 7335 973E 0A49 47FC  0A8C 38D5 96BE 07D3 34AB")))))
+                     "3FFA 7335 973E 0A49 47FC  0A8C 38D5 96BE 07D3 34AB"))))
+                (channel
+                  (name 'sops-guix)
+                  (url "https://github.com/fishinthecalculator/sops-guix")
+                  (branch "main")
+                  (introduction
+                   (make-channel-introduction
+                    "0bbaf1fdd25266c7df790f65640aaa01e6d2dbc9"
+                    (openpgp-fingerprint
+                     "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2")))))
 
           %default-channels))
