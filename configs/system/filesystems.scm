32f020e267cd84cac92ff573e498e6cf6756c464
BrokenShine <brokenshine@noreply.codeberg.org>
2026-01-18 13:55:25 +0800

    merged these two files.

--- configs/system/filesystems.scm
+++ configs/system/filesystems.scm
@@ -1,3 +1,10 @@
+(define %mapped-devices-config
+  (list (mapped-device
+          (source (uuid "327f2e02-1e4f-48b2-87f0-797c481850c9"))
+          (target "root")
+          (type luks-device-mapping)
+          (arguments '(#:key-file "/cryptroot.key")))))
+
 (define %file-systems-config
   (append (list (file-system
                   (device (file-system-label "Linux"))
--- configs/system/luks.scm
+++
@@ -1,6 +0,0 @@
-(define %mapped-devices-config
-  (list (mapped-device
-          (source (uuid "327f2e02-1e4f-48b2-87f0-797c481850c9"))
-          (target "root")
-          (type luks-device-mapping)
-          (arguments '(#:key-file "/cryptroot.key")))))
