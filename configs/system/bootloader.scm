9b0da0119e26ed57b58194d66e668d34545b8f04
BrokenShine <brokenshine@noreply.codeberg.org>
2026-01-18 15:34:08 +0800

    using grub.

--- configs/system/bootloader.scm
+++ configs/system/bootloader.scm
@@ -1,6 +1,6 @@
 (define %bootloader-config
   (bootloader-configuration
-    (bootloader uefi-uki-removable-bootloader)
+    (bootloader grub-efi-luks2-bootloader)
     (theme (grub-theme (inherit (grub-theme))
                        (gfxmode '("1024x786x32"))))
     (targets '("/efi"))
