1865250a83ff5fa8f31768bee5a374b23d4d5e6d
BrokenShine <brokenshine@noreply.codeberg.org>
2026-01-21 02:26:12 +0800

    try to add nix.

--- configs/system/modules.scm
+++ configs/system/modules.scm
@@ -1,10 +1,5 @@
 (use-modules (gnu)
-             (gnu home services guix)
              (gnu system nss)
              (gnu system accounts)
-
-             (guix channels)
-
              (nonguix transformations)
-
              (rosenthal))
