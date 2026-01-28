a1b71cf3927a60144992841468665b7a09ca982e
BrokenShine <brokenshine@noreply.codeberg.org>
2026-01-28 22:18:00 +0800

    disabled root account.

--- configs/system/users.scm
+++ configs/system/users.scm
@@ -11,16 +11,20 @@
 (define %host-name-config
   "BrokenShine-Desktop")
 (define %users-config
-  (cons (user-account
-          (name username)
-          (group "users")
-          (password
-           "$6$C2H4Td9gJHEa4qFi$fN.tnh2XibU1aqHpwcq.zewxyMeHR83EyP0r8UROzjj6l88VijpOogCbVarmrlCnig8k967wT7ifcJAZunZ.l.")
-          (supplementary-groups '("audio"
-                                  "cgroup"
-                                  "kvm"
-                                  "libvirt"
-                                  "netdev"
-                                  "video"
-                                  "wheel"))
-          (shell (file-append fish "/bin/fish"))) %base-user-accounts))
+  (cons* (user-account
+           (inherit %root-account)
+           (password #f)
+           (shell (file-append (spec->pkg "fish") "/bin/fish")))
+         (user-account
+           (name username)
+           (group "users")
+           (password
+            "$6$C2H4Td9gJHEa4qFi$fN.tnh2XibU1aqHpwcq.zewxyMeHR83EyP0r8UROzjj6l88VijpOogCbVarmrlCnig8k967wT7ifcJAZunZ.l.")
+           (supplementary-groups '("audio"
+                                   "cgroup"
+                                   "kvm"
+                                   "libvirt"
+                                   "netdev"
+                                   "video"
+                                   "wheel"))
+           (shell (file-append (spec->pkg "fish") "/bin/fish"))) %base-user-accounts))
