ddd52cdac7a8b158b340c8f39ae6aafceaf98e66
BrokenShine <brokenshine@noreply.codeberg.org>
2026-01-30 03:08:04 +0800

    changed guix's tmpdir.

--- configs/system/services.scm
+++ configs/system/services.scm
@@ -1,4 +1,4 @@
-;;; SPDX-FileCopyrightText: 2026 Copyright (C) 2024-2026 BrokenShine <xchai404@gmail.com>
+;;; SPDX-FileCopyrightText: 2026 BrokenShine <xchai404@gmail.com>
 ;;;
 ;;; SPDX-License-Identifier: GPL-3.0
 
@@ -311,7 +311,8 @@
                                                    (http-proxy
                                                     "http://127.0.0.1:7890")
                                                    (discover? #f)
-                                                   (privileged? #f)))
+                                                   (privileged? #f)
+                                                   (tmpdir "/var/tmp")))
 
             (udev-service-type config =>
                                (udev-configuration (inherit config)
