e3ce881115198d1e1e6bbea27076216f4b5e112b
BrokenShine <brokenshine@noreply.codeberg.org>
2026-01-30 03:07:11 +0800

    removed fish configs.

--- configs/home/services/dotfile.scm
+++ configs/home/services/dotfile.scm
@@ -1,6 +1,9 @@
-;;; SPDX-FileCopyrightText: 2026 Copyright (C) 2024-2026 BrokenShine <xchai404@gmail.com>
+;;; SPDX-FileCopyrightText: 2026 BrokenShine <xchai404@gmail.com>
 ;;;
 ;;; SPDX-License-Identifier: GPL-3.0
+
+(use-modules (gnu home services shells)
+             (rosenthal services shellutils))
 
 (use-package-modules shells)
 
@@ -17,13 +20,4 @@
 
         (service home-xdg-configuration-files-service-type
                  `(("gdb/gdbinit" ,%default-gdbinit)
-                   ("nano/nanorc" ,%default-nanorc)))
-
-        (simple-service 'fish-foreign-env
-                        home-xdg-configuration-files-service-type
-                        `(("fish/conf.d/01-fish-foreign-env-main.fish" ,(file-append
-                                                                         fish-foreign-env
-                                                                         "/share/fish/functions/fenv.main.fish"))
-                          ("fish/conf.d/02-fish-foreign-env.fish" ,(file-append
-                                                                    fish-foreign-env
-                                                                    "/share/fish/functions/fenv.fish"))))))
+                   ("nano/nanorc" ,%default-nanorc)))))
