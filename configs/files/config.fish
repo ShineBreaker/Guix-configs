0cc2d59cc926539ce93607ec7528da64439e537f
BrokenShine <brokenshine@noreply.codeberg.org>
2026-01-30 02:55:47 +0800

    combine fish configs.

---
+++ configs/files/config.fish
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: 2026 Copyright (C) 2024-2026 BrokenShine <xchai404@gmail.com>
+#
+# SPDX-License-Identifier: GPL-3.0
+
+# status --is-login; and not set -q __fish_login_config_sourced
+# and begin
+#     fenv . $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+# end
+
+function __fastfetch_on_startup --on-event fish_prompt
+    functions -e __fastfetch_on_startup
+    fastfetch
+    echo \n日々私たちが過ごしている日常は、実は、奇跡の連続なのかもしれない。\n | lolcat
+end
+
+fzf --fish | source
+starship init fish | source
--- dotfiles/.config/fish/conf.d/99-foreign-envs.fish
+++
@@ -1,5 +0,0 @@
-# SPDX-FileCopyrightText: 2026 Copyright (C) 2024-2026 BrokenShine <xchai404@gmail.com>
-#
-# SPDX-License-Identifier: GPL-3.0
-
-fenv . $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
