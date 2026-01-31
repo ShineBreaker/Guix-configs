ed52de87494783a61ec6901fb026d79de786b000
BrokenShine <brokenshine@noreply.codeberg.org>
2026-01-31 20:02:50 +0800

    seperate /tmp.

--- configs/system/filesystems.scm
+++ configs/system/filesystems.scm
@@ -13,13 +13,20 @@
           (type luks-device-mapping)
           (arguments '(#:key-file "/cryptroot.key")))))
 
-(define %tmpfs-on-root
-  (file-system
-    (mount-point "/")
-    (device "tmpfs")
-    (type "tmpfs")
-    (options "mode=0755,nr_inodes=1m,size=25%")
-    (check? #f)))
+(define %tmpfs
+  (list (file-system
+          (mount-point "/")
+          (device "tmpfs")
+          (type "tmpfs")
+          (options "mode=0755,nr_inodes=1m,size=10%")
+          (check? #f))
+        (file-system
+          (mount-point "/tmp")
+          (device "tmpfs")
+          (type "tmpfs")
+          (flags '(no-dev no-suid no-atime))
+          (options "mode=1777,nr_inodes=1m,size=75%")
+          (check? #f))))
 
 (define %persist-filesystem
   (map (match-lambda
@@ -31,10 +38,8 @@
             (dependencies %mapped-devices-config)
             (options (string-append "subvol=" subvol ",compress=zstd:6"))
             (create-mount-point? #t)
-            (needed-for-boot?
-             (member mount-point '("/gnu" "/var/guix")))
-            (check? (string=? mount-point "/gnu")))))
-       %btrfs-subvolumes))
+            (needed-for-boot? #t)
+            (check? (string=? mount-point "/gnu"))))) %btrfs-subvolumes))
 
 (define %data-filesystem
   (file-system
@@ -47,25 +52,20 @@
 
 (define %bind-mounts
   (map (lambda (dirs)
-           (file-system
-             (mount-point (string-append "/home/" username "/" dirs))
-             (device (string-append "/data/" dirs))
-             (type "none")
-             (flags '(bind-mount))
-             (dependencies (list %data-filesystem))
-             (create-mount-point? #t)))
-  %data-dirs))
+         (file-system
+           (mount-point (string-append "/home/" username "/" dirs))
+           (device (string-append "/data/" dirs))
+           (type "none")
+           (flags '(bind-mount))
+           (dependencies (list %data-filesystem))
+           (create-mount-point? #t))) %data-dirs))
 
 (define %file-systems-config
-  (append
-    %persist-filesystem
-    %bind-mounts
-    (list
-     %tmpfs-on-root
-     %data-filesystem
-     (file-system
-       (device (uuid "9699-52A2" 'fat))
-       (mount-point "/efi")
-       (type "vfat")
-       (create-mount-point? #t)))
-   %base-file-systems))
+  (append %bind-mounts %persist-filesystem %tmpfs
+          (list %data-filesystem
+                (file-system
+                  (device (uuid "9699-52A2"
+                                'fat))
+                  (mount-point "/efi")
+                  (type "vfat")
+                  (create-mount-point? #t))) %base-file-systems))
