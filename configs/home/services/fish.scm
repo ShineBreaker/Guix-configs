86b2d010931be8664667238df103319b26d38810
BrokenShine <brokenshine@noreply.codeberg.org>
2026-01-31 20:02:06 +0800

    using specs->pkgs+out.

--- configs/home/services/fish.scm
+++ configs/home/services/fish.scm
@@ -3,15 +3,15 @@
 ;;; SPDX-License-Identifier: GPL-3.0
 
 (define %fish-packages-list
-  (specifications->packages (list "atuin"
-                                  "bat"
-                                  "direnv"
-                                  "fd"
-                                  "ripgrep"
-                                  "fzf"
-                                  "lolcat"
-                                  "starship"
-                                  "zoxide")))
+  (cons* (specs->pkgs+out "atuin"
+                          "bat"
+                          "direnv"
+                          "fd"
+                          "ripgrep"
+                          "fzf"
+                          "lolcat"
+                          "starship"
+                          "zoxide")))
 
 (define %fish-services
   (list (service home-fish-plugin-atuin-service-type)
@@ -24,16 +24,17 @@
                                                      ("grep" . "rg")
                                                      ("htop" . "btop")
                                                      ("ll" . "ls -la")
-                                                     ("rm" . "rm -i")))
+                                                     ("rm" . "rm -i")
+                                                     ("sudo" . "/run/privileged/bin/sudo")))
 
                                           (abbreviations '(("commit" . "'git commit --all -S'")
                                                            ("enter" . "distrobox enter")
                                                            ("push" . "git push")
                                                            ("reboot" . "loginctl reboot")
-                                                           ("rebuild" . "'sudo guix system reconfigure ./config.scm && guix home reconfigure ./home-config.scm'")
+                                                           ("rebuild" . "'sudo guix system reconfigure ./config.scm --allow-downgrades && guix home reconfigure ./home-config.scm --allow-downgrades'")
                                                            ("shutdown" . "loginctl poweroff")
                                                            ("update" . "'sudo flatpak upgrade -y && flatpak upgrade -y && distrobox upgrade'")
-                                                           ("upgrade" . "guix pull")))
+                                                           ("upgrade" . "'guix pull -C ./channel.scm && guix describe --format=channels > channel.lock'")))
 
                                           (config (list (local-file
                                                          "../../files/config.fish")))))))
