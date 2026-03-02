c90eb2c486fc12637603b00f13bd8c22576127ff
BrokenShine <brokenshine@noreply.codeberg.org>
2026-03-02 20:54:01 +0800

    unified udev rules.

--- configs/main/system-config.scm
+++ configs/main/system-config.scm
@@ -35,9 +35,5 @@
 
   (services %services-config)
   (skeletons %skeletons-config)
-  (groups (append (list (user-group
-                           (name "adbusers")
-                           (system? #t)))
-                    %base-groups))
 
   (name-service-switch %mdns-host-lookup-nss))
--- configs/system/services.scm
+++ configs/system/services.scm
@@ -58,6 +58,21 @@
                 (service rtkit-daemon-service-type)
                 (service tailscale-service-type)
 
+                ;; udev rules
+                (udev-rules-service 'android android-udev-rules
+                                    #:groups '("adbusers"))
+                (udev-rules-service 'opentabletdriver
+                                    opentabletdriver-udev-rules)
+                (udev-rules-service 'steam-devices steam-devices-udev-rules)
+                (udev-rules-service 'controller
+                                    (udev-rule
+                                     "60-controller-permission.rules"
+                                     "KERNEL==\"event*\", ATTRS{idVendor}==\"045e\", ATTRS{idProduct}==\"028e\", MODE=\"0660\", GROUP=\"users\""))
+                (udev-rules-service 'cpu-dma
+                                    (udev-rule "99-cpu-dma-latency.rules"
+                                     "DEVPATH==\"/devices/virtual/misc/cpu_dma_latency\", OWNER=\"root\", GROUP=\"audio\", MODE=\"0660\""))
+
+                ;; System related
                 (simple-service 'home-channels home-channels-service-type
                                 guix-channels)
 
