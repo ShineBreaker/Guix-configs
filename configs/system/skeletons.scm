4cf67d847b9134a31c5d9926abd5b8b2f6af2cd5
BrokenShine <brokenshine@noreply.codeberg.org>
2026-03-07 21:54:18 +0800

    Update skeletons.scm

--- configs/system/skeletons.scm
+++ configs/system/skeletons.scm
@@ -1,2 +1,3 @@
 (define %skeletons-config
-  `((".config/mihomo/config.yaml" ,(local-file "../configs/files/mihomo.yaml"))))
+  `((".config/mihomo/config.yaml" ,(local-file "../configs/files/mihomo.yaml"))
+    (".config/noctalia/settings.json" ,(local-file "../configs/files/noctalia.json"))))
