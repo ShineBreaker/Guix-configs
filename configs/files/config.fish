e83ee8bf160d7d1c77669c65dab796ee0722f48f
BrokenShine <brokenshine@noreply.codeberg.org>
2026-03-07 21:54:52 +0800

    Update config.fish

--- configs/files/config.fish
+++ configs/files/config.fish
@@ -3,15 +3,16 @@
 # SPDX-License-Identifier: GPL-3.0
 
 function __fastfetch_on_startup --on-event fish_prompt
+
     functions -e __fastfetch_on_startup
     $$bin/fastfetch$$
     echo \n日々私たちが過ごしている日常は、実は、奇跡の連続なのかもしれない。\n | $$bin/lolcat$$
+
 end
 
-status --is-login; and not set -q __fish_login_config_sourced
-and begin
+if status is-interactive
 
-	  fenv . $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+    fenv . $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
 		fenv source /etc/environment
 
 end
