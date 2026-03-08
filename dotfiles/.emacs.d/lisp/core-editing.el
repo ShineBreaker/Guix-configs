573fe1eae45370a48b4f8a65eee1e461c8229174
BrokenShine <brokenshine@noreply.codeberg.org>
2026-03-09 00:55:14 +0800

    using codex to modified emacs configs.

---
+++ dotfiles/.emacs.d/lisp/core-editing.el
@@ -0,0 +1,46 @@
+;;; core-editing.el --- 编辑行为与代码可读性 -*- lexical-binding: t; -*-
+
+;;; Code:
+
+;; 通用编辑习惯：更接近现代 IDE 的默认体验。
+(setq-default indent-tabs-mode nil
+              tab-width 2
+              fill-column 100
+              truncate-lines t
+              word-wrap nil)
+
+(electric-pair-mode 1)
+(show-paren-mode 1)
+(delete-selection-mode 1)
+(global-auto-revert-mode 1)
+
+(use-package ws-butler
+  :hook ((text-mode . ws-butler-mode)
+         (prog-mode . ws-butler-mode)))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; 复刻 Zed 的 git gutter tracked_files。
+(use-package diff-hl
+  :hook ((prog-mode . diff-hl-mode)
+         (text-mode . diff-hl-mode)
+         (dired-mode . diff-hl-dired-mode))
+  :config
+  (global-diff-hl-mode 1))
+
+;; 近似 Zed inline blame：按行弹出最近提交信息。
+(use-package git-messenger
+  :bind (("C-c g b" . git-messenger:popup-message))
+  :custom
+  (git-messenger:show-detail t)
+  (git-messenger:use-magit-popup t))
+
+;; 近似 Zed sticky scroll：显示当前函数头。
+(use-package stickyfunc-enhance
+  :hook (prog-mode . semantic-stickyfunc-mode)
+  :config
+  (semantic-mode 1))
+
+(provide 'core-editing)
+;;; core-editing.el ends here
