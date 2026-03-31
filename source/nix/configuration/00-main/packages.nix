b9f3e69620a3e653f69d4c6ec35c365a942ea3e2
BrokenShine <brokenshine@noreply.codeberg.org>
2026-03-31 08:00:26 +0800

    Update packages.nix

--- source/nix/configuration/00-main/packages.nix
+++ source/nix/configuration/00-main/packages.nix
@@ -17,8 +17,14 @@
     gamescope
 
     pmbootstrap
-
     sbctl
+
+    (qq.override {
+      commandLineArgs = "--enable-wayland-ime --wayland-text-input-version=3";
+    })
+    wechat
+
+    postman
 
     libsForQt5.qt5ct
     kdePackages.qt6ct
