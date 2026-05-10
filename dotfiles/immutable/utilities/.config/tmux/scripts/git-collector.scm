298eba1c23418966871349faca40bfb603fd2949
BrokenShine <brokenshine@noreply.codeberg.org>
2026-05-10 21:26:09 +0800

    REMOVE: (tmux) 移除了一些没用的东西

--- dotfiles/immutable/utilities/.config/tmux/scripts/git-collector.scm
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env guile
-!#
-
-;; Compatibility wrapper. The sidebar implementation lives in sidebar-render.scm.
-
-(let ((script (string-append (getenv "HOME") "/.config/tmux/scripts/sidebar-render.scm")))
-  (setenv "GUILE_AUTO_COMPILE" "0")
-  (execlp "guile" "guile" "--no-auto-compile" "-s" script "git"))
