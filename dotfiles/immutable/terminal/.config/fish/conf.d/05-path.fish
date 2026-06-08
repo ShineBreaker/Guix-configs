65e6574da95f50d465b5e7838d230812fb9b4bca
BrokenShine <brokenshine@noreply.codeberg.org>
2026-06-08 19:10:26 +0800

    UPDATE: (05-path.fish) try to fix nix.

--- dotfiles/immutable/terminal/.config/fish/conf.d/05-path.fish
+++ dotfiles/immutable/terminal/.config/fish/conf.d/05-path.fish
@@ -3,13 +3,14 @@
 # SPDX-License-Identifier: MIT
 
 ## Nix
-# fenv . $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+fenv . $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+fish_add_path -g -a $HOME/.nix-profile/bin
 
 ## PNPM
 mkdir -p $HOME/.local/share/pnpm
 set -gx PNPM_HOME $HOME/.local/share/pnpm
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 
 ## User
