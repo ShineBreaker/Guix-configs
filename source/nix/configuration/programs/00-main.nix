446127dd0b20712dd052d97c0f9a83a3a58cfd5a
BrokenShine <brokenshine@noreply.codeberg.org>
2026-07-09 01:10:17 +0800

    UPDATE: moved more packages into `with-nix-profile`

--- source/nix/configuration/programs/00-main.nix
+++ source/nix/configuration/programs/00-main.nix
@@ -3,23 +3,12 @@
 {
   imports = [
     ./hermes.nix
-    ./zed.nix
+    # ./zed.nix
   ];
 
   home.packages = with pkgs; [
     ## Tools
     python314Packages.jieba
-
-    ## Android
-    android-tools
     qtscrcpy
-
-    ## LSP
-    bash-language-server
-    kotlin-language-server
-    nil
-    package-version-server
-    typescript-language-server
   ];
-
 }
