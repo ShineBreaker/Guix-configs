0dd749f4686da2c104d11a200ab1981ecc0ad4f3
BrokenShine <brokenshine@noreply.codeberg.org>
2026-07-14 19:55:35 +0800

    UPDATE: moved some packages into `nix` & try to fix bugs.

--- source/nix/configuration/programs/00-main.nix
+++ source/nix/configuration/programs/00-main.nix
@@ -3,15 +3,43 @@
 {
   imports = [
     ./hermes.nix
-    # ./zed.nix
+    ./zed.nix
   ];
 
   home.packages = with pkgs; [
     ## Tools
-    claude-code
-    claude-agent-acp
+    broot
+    gh
     python314Packages.jieba
+
+    ## Android
+    android-tools
+    pmbootstrap
     qtscrcpy
+    scrcpy
+
+    ## Gaming Tools
+    gamescope
+    mangojuice
+    sunshine
+
+    ## Environment management
+    biome
+    bun
+    pnpm
+
+    ## LSP
+    bash-language-server
+    kotlin-language-server
+    nil
+    package-version-server
+    typescript-language-server
+    vscode-langservers-extracted
+
+    # AI Agent
+    claude-code
+    codex
+    codex-acp
 
     (qq.override {
       commandLineArgs = "--ozone-platform=wayland --enable-features=UseOzonePlatform,WaylandWindowDecorations --enable-wayland-ime --wayland-text-input-version=3";
